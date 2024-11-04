eec9edf55244bc9e509ffcd31ea7b0dc3412749b
IMYONGJIN\dksp5 <dksp56@naver.com>
2024-11-05 04:33:49 +0900

    11-05

--- src/main/java/com/memo/post/mapper/PostMapper.java
+++ src/main/java/com/memo/post/mapper/PostMapper.java
@@ -5,9 +5,17 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.memo.post.domain.Post;
+
 @Mapper
 public interface PostMapper {
 
-	// input: X		output: List<Map<String , Objcet>>
+	// input: X		
+	// output: List<Map<String , Objcet>>
 	public List<Map<String , Object>> selectPostList();
+	
+	// input: userId
+	// output: List<post>
+
+	public List<Post> selectPostListByUserId(int userId);
 }
